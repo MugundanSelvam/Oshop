c8af17a3e8402d271d44cd84ccd24a96bf5b3734
Mugundan <mugu2804@gmail.com>
2019-03-04 15:20:51 +0530

    First

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDmdUcn8Mx8Vu_PgeCOBiNUzl1Rh8_k4gI",
+    authDomain: "mugustore-52495.firebaseapp.com",
+    databaseURL: "https://mugustore-52495.firebaseio.com",
+    projectId: "mugustore-52495",
+    storageBucket: "mugustore-52495.appspot.com",
+    messagingSenderId: "1038809441022"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDmdUcn8Mx8Vu_PgeCOBiNUzl1Rh8_k4gI",
+    authDomain: "mugustore-52495.firebaseapp.com",
+    databaseURL: "https://mugustore-52495.firebaseio.com",
+    projectId: "mugustore-52495",
+    storageBucket: "mugustore-52495.appspot.com",
+    messagingSenderId: "1038809441022"
+  }
 };
 
 /*
